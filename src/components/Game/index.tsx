e4476d173942d9a15a93a5840ff81ae21fda9b74
Tristan Siegel <tristan.t.siegel@gmail.com>
2020-02-12 10:51:41 -0500

    WIP on basic options

--- src/components/Game/index.tsx
+++ src/components/Game/index.tsx
@@ -2,27 +2,56 @@ import React from 'react'
 import './style.css'
 import Board from '../Board'
 import { Board as BoardType } from '../../utils/types'
+import Options from '../Options'
 
 type GameState = {
+  hasLost: boolean
+  hasTied: boolean
+  hasGameStarted: boolean
   board: BoardType
+}
+
+function blankState(): GameState {
+  return {
+    hasLost: false,
+    hasTied: false,
+    hasGameStarted: false,
+    board: [
+      ['', '', ''],
+      ['', '', ''],
+      ['', '', '']
+    ]
+  }
 }
 
 export default class Game extends React.Component<{}, GameState> {
   constructor(props: any) {
     super(props)
-    this.state = {
-      board: [
-        ['', '', ''],
-        ['', '', ''],
-        ['', '', '']
-      ]
-    }
+    this.state = blankState()
+  }
+
+  resetGame() {
+    this.setState(blankState())
+  }
+
+  startGame(player: 'computer' | 'human') {
+    console.log(player)
+    this.setState({ hasGameStarted: true })
   }
 
   render() {
-    const { board } = this.state
+    const { board, hasGameStarted } = this.state
+    const resetGame = this.resetGame.bind(this)
+    const startGame = this.startGame.bind(this)
     return (
-      <Board board={board}/>
+      <div>
+        <Board board={board}/>
+        <Options
+          hasGameStarted={hasGameStarted}
+          resetGame={() => resetGame()}
+          startGame={(choice) => startGame(choice)}
+        />
+      </div>
     )
   }
 }
--- src/components/Options/index.tsx
+++ src/components/Options/index.tsx
@@ -1,5 +1,34 @@
 import React from 'react'
+import { ReactSVG } from 'react-svg'
 import './style.css'
 
-export default class Options extends React.Component {
+type OptionsProps = {
+  hasGameStarted: boolean
+  resetGame: () => any
+  startGame: (choice: 'computer' | 'human') => any
+}
+
+export default class Options extends React.Component<OptionsProps> {
+  render() {
+    const { hasGameStarted, resetGame, startGame } = this.props
+    if (hasGameStarted) {
+      return (
+        <div className="game-choices twelve columns">
+          <button onClick={resetGame}>Start Over</button>
+        </div>
+      )
+    } else {
+      return (
+        <div className="game-choices twelve columns">
+          <span className="twelve columns">Who goes first?</span>
+          <div className="six columns" onClick={() => startGame('computer')}>
+            <ReactSVG src='robot.svg' className='computer'/>
+          </div>
+          <div className="six columns" onClick={() => startGame('human')}>
+            <ReactSVG src='human.svg' className='human'/>
+          </div>
+        </div>
+      )
+    }
+  }
 }
