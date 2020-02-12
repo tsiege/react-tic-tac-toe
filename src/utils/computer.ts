69ccbeec2e4e213a31f9710f0e6b8de985d2f2fc
Tristan Siegel <tristan.t.siegel@gmail.com>
2020-02-12 11:52:11 -0500

    basic computer

--- src/utils/computer.ts
+++ src/utils/computer.ts
@@ -1,3 +1,17 @@
-export function computer () {
+import { Board, Move } from './types'
 
+export function getComputerMove (board: Board) {
+  const emptySpaces = getEmptySpaces(board)
+  const i = Math.floor(Math.random() * getEmptySpaces(board).length)
+  return emptySpaces[i]
+}
+
+function getEmptySpaces(board: Board) {
+  const emptySpaces: Move[] = []
+  for (let i = 0; i < board.length; i++) {
+    if (board[i] === '') {
+      emptySpaces.push(i as Move)
+    }
+  }
+  return emptySpaces
 }
--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,5 +1,9 @@
+type tile = '' | 'X' | 'O'
+
 export type Board = [
-  [string, string, string],
-  [string, string, string],
-  [string, string, string]
+  tile, tile, tile,
+  tile, tile, tile,
+  tile, tile, tile
 ]
+
+export type Move = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8
