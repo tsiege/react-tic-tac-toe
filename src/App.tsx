9679ae4c2f62f5b2f220e37aa76a0ed27c44b571
Tristan Siegel <tristan.t.siegel@gmail.com>
2020-02-12 10:51:11 -0500

    WIP basic styling

--- src/App.tsx
+++ src/App.tsx
@@ -6,20 +6,11 @@ import Game from './components/Game'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <header className="App-header">
+        <h1>
+          Tic Tac Toe
+        </h1>
+      </header> */}
       <Game />
     </div>
   )
