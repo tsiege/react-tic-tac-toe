416d45ed544c3f91e36a222df81361b3a04b1e5c
Tristan Siegel <tristan.t.siegel@gmail.com>
2020-03-26 10:33:05 -0400

    track build for static hosting

---
+++ build/precache-manifest.dc1a737159ec6137e51dd834aae4647b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "804b5eea96163cc2fd01c197ca389529",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7fa1c985e87d352f86b1",
+    "url": "/static/css/main.7cb2452e.chunk.css"
+  },
+  {
+    "revision": "0e04ddf82ccb08fc95b2",
+    "url": "/static/js/2.8efeaa2c.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.8efeaa2c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7fa1c985e87d352f86b1",
+    "url": "/static/js/main.731be5ea.chunk.js"
+  },
+  {
+    "revision": "c09018f4f18115bcfb3c",
+    "url": "/static/js/runtime-main.0897c87d.js"
+  }
+]);
