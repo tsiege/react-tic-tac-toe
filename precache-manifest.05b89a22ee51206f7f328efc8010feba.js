9b783c3d6e92686eccb2f5e36e1c16645b1980bd
Tristan Siegel <tristan.t.siegel@gmail.com>
2020-03-26 12:31:46 -0400

    Updates

---
+++ precache-manifest.05b89a22ee51206f7f328efc8010feba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5f8528ef21e32dfe1e1ab16cbc64f77",
+    "url": "/react-tic-tac-toe/index.html"
+  },
+  {
+    "revision": "7fa1c985e87d352f86b1",
+    "url": "/react-tic-tac-toe/static/css/main.7cb2452e.chunk.css"
+  },
+  {
+    "revision": "0e04ddf82ccb08fc95b2",
+    "url": "/react-tic-tac-toe/static/js/2.8efeaa2c.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react-tic-tac-toe/static/js/2.8efeaa2c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7fa1c985e87d352f86b1",
+    "url": "/react-tic-tac-toe/static/js/main.731be5ea.chunk.js"
+  },
+  {
+    "revision": "1762ced207f918761cfc",
+    "url": "/react-tic-tac-toe/static/js/runtime-main.fb6ba11d.js"
+  }
+]);
